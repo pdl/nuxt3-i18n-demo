779fe77818a0ce03720148bdad26b53f90907559
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2023-12-07 10:44:29 +0000

    fix: moving the config to nuxt.config.ts causes routing to work

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,25 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/i18n"],
+  i18n: {
+    lazy: true,
+    langDir: "locales",
+    defaultLocale: "en",
+    locales: [
+      {
+        name: "English",
+        code: "en",
+        iso: "en-GB",
+        file: "en.json",
+      },
+      {
+        name: "Français",
+        code: "fr",
+        iso: "fr-FR",
+        file: "fr.json",
+      },
+    ],
+    strategy: "prefix",
+    detectBrowserLanguage: false,
+  }
 })
