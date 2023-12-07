e25ba9f85be643fa15a6a33405caee5ce4ec0aa1
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2023-12-07 10:47:30 +0000

    Configuration in modules also works

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,27 @@
+const i18nConfig = {
+  lazy: true,
+  langDir: "locales",
+  defaultLocale: "en",
+  locales: [
+    {
+      name: "English",
+      code: "en",
+      iso: "en-GB",
+      file: "en.json",
+    },
+    {
+      name: "Français",
+      code: "fr",
+      iso: "fr-FR",
+      file: "fr.json",
+    },
+  ],
+  strategy: "prefix",
+  detectBrowserLanguage: false,
+}
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/i18n"],
-  i18n: {
-    lazy: true,
-    langDir: "locales",
-    defaultLocale: "en",
-    locales: [
-      {
-        name: "English",
-        code: "en",
-        iso: "en-GB",
-        file: "en.json",
-      },
-      {
-        name: "Français",
-        code: "fr",
-        iso: "fr-FR",
-        file: "fr.json",
-      },
-    ],
-    strategy: "prefix",
-    detectBrowserLanguage: false,
-  }
+  modules: [["@nuxtjs/i18n", i18nConfig]],
 })
